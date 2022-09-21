78208b718f7f5c1de4e6418a55e7eb8b9ec9f4cc
Wasif Shah <wasif1024@gmail.com>
2022-09-22 03:52:09 +0500

    latest

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+pub trait Vehicle {
+
+    fn forward() -> String;
+    fn backward() -> String;
+
+    fn turn_ignition() -> String {
+        String::from("vroom vroom")
+    }
+}
+
+pub struct Car {
+    color: String,
+}
+
+impl Vehicle for Car {
+    fn forward() -> String {
+        String::from("Driving my car forward")
+    }
+    fn backward() -> String {
+        String::from("Backing my car up...")
+    }
+}
+
+pub struct Truck {
+    color: String,
+}
+
+impl Vehicle for Truck {
+    fn forward() -> String {
+        String::from("Driving my truck forward")
+    }
+    fn backward() -> String {
+        String::from("Backing my truck up...")
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", Car::turn_ignition());
+    println!("{}", Car::forward());
+    println!("{}", Car::backward());
+    println!("{}", Truck::turn_ignition());
+    println!("{}", Truck::forward());
+    println!("{}", Truck::backward());
 }
